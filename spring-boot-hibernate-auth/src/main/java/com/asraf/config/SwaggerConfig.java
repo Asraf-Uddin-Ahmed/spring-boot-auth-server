bd11a6bf216323f6f9fbb40318adf67f6af011b8
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2019-04-08 15:41:36 +0600

    Pageable injection problem fixed

--- spring-boot-hibernate-auth/src/main/java/com/asraf/config/SwaggerConfig.java
+++ spring-boot-hibernate-auth/src/main/java/com/asraf/config/SwaggerConfig.java
@@ -3,7 +3,7 @@ package com.asraf.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport  {                                    
+public class SwaggerConfig implements WebMvcConfigurer  {                                    
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)
