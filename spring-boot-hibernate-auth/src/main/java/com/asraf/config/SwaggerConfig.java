1cf0fcf054aef81cd7713159986751cb078b81e8
Asraf Uddin Ahmed <aahmed@ejogajogbd.com>
2019-04-08 16:44:36 +0600

    API info added to swagger

--- spring-boot-hibernate-auth/src/main/java/com/asraf/config/SwaggerConfig.java
+++ spring-boot-hibernate-auth/src/main/java/com/asraf/config/SwaggerConfig.java
@@ -1,5 +1,7 @@
 package com.asraf.config;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,6 +9,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -17,10 +21,25 @@ public class SwaggerConfig implements WebMvcConfigurer  {
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)
-          .select()                                  
+          .select()
           .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+          //.apis(RequestHandlerSelectors.basePackage("com.asraf.controllers"))
+          //.apis(RequestHandlerSelectors.basePackage("org.springframework.security.oauth2.provider.endpoint"))
+          .paths(PathSelectors.any())
+          .build()
+          .apiInfo(apiInfo());
+    }
+    
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+          "My REST API",
+          "Some custom description of API.", 
+          "v1.0",
+          "Terms of service URL", 
+          new Contact("Asraf Uddin Ahmed (Ratul)", "https://github.com/Asraf-Uddin-Ahmed", "13ratul@gmail.com"), 
+          "License of API",
+          "API license URL", 
+          Collections.emptyList());
     }
     
     @Override
